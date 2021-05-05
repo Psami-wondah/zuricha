2bbd7f5267a49ecc9e5aa546b6d413ab582552f0
Psami-wondah <okechukwusamuel16@gmail.com>
2021-05-05 11:30:17 +0100

    oh mehn

--- zurichat/asgi.py
+++ zurichat/asgi.py
@@ -11,8 +11,8 @@ django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
-from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
@@ -20,8 +20,8 @@ application = ProtocolTypeRouter({
 
     # WebSocket chat handler
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            url(r"^chat/$", ChatConsumer.as_asgi()),
-        ])
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
