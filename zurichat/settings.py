3655e7b5f82678f73edd2006e365b2e3602528cb
Psami-wondah <okechukwusamuel16@gmail.com>
2021-05-05 10:51:17 +0100

    oh mehn

--- zurichat/settings.py
+++ zurichat/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import os, environ
 from pathlib import Path
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
