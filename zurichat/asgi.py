4c05b420f6ac6237e5d606390da5aa1586d4e269
Psami-wondah <okechukwusamuel16@gmail.com>
2021-05-05 11:23:06 +0100

    oh mehn

--- zurichat/asgi.py
+++ zurichat/asgi.py
@@ -1,28 +1,27 @@
-"""
-ASGI config for zurichat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-import django
 
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
-from channels.auth import AuthMiddlewareStack
-import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zurichat.settings')
-django.setup()
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat.consumers import ChatConsumer
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^chat/$", ChatConsumer.as_asgi()),
+        ])
     ),
 })
-
