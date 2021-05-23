46346e59031826868c7615256479a0c380c681b0
Psami-wondah <okechukwusamuel16@gmail.com>
2021-05-23 21:04:40 +0100

    css and html shii

--- zurichat/settings.py
+++ zurichat/settings.py
@@ -80,27 +80,27 @@ WSGI_APPLICATION = 'zurichat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('ENGINE'),
-        'NAME': os.getenv('NAME'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        },
-
-
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': os.getenv('ENGINE'),
+#         'NAME': os.getenv('NAME'),
+#         'HOST': os.getenv('HOST'),
+#         'PORT': os.getenv('PORT'),
+#         'USER': os.getenv('USER'),
+#         'PASSWORD': os.getenv('PASSWORD'),
+#         'TEST': {
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         },
+#
+#
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
